1564e29fae379c990c1967866d248818040aedfc
eversonmariano <mariano.computacao@gmail.com>
2024-10-07 21:51:30 -0300

    Criando e estilizando o component User e preparando dados dos usuarios

---
+++ starting-project/src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Yasmim Maria',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Emily Silva',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Marcus José',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'David Paulo',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Pedro Higor',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Roberto Carlos',
+    avatar: 'user-6.jpg',
+  },
+];
